f5797e39618a02d241dfa53daa0e771480409333
Eduard <shartner2006@mail.ru>
2024-12-10 17:07:44 +0500

    some changes in cards

--- src/pages/autopark.vue
+++ src/pages/autopark.vue
@@ -14,24 +14,26 @@ function pushRouter(car: Car) {
 </script>
 
 <template>
-  <div class="mt-10 flex justify-evenly">
-    <CardComponent v-for="car in cars" :key="car.id" class="w-1/4" @click="pushRouter(car)">
-      <template #header>
-        <div class="flex flex-col items-center">
-          <img :src="car.image" class="h-xl w-full">
-          <h1 class="text-xl">
-            {{ car.model }}
-          </h1>
-        </div>
-      </template>
-      <template #body>
-        <p>{{ car.manufactureYear }}</p>
-      </template>
-      <template #footer>
-        <div class="flex justify-center">
-          ujnjdj
-        </div>
-      </template>
-    </CardComponent>
+  <div class="m-auto container">
+    <div class="flex flex-wrap justify-between">
+      <CardComponent v-for="car in cars" :key="car.id" class="mt-10 w-[30%]" @click="pushRouter(car)">
+        <template #header>
+          <div class="flex flex-col items-center">
+            <img :src="car.image" class="h-xl w-full">
+            <h1 class="text-xl">
+              {{ car.model }}
+            </h1>
+          </div>
+        </template>
+        <template #body>
+          <p>{{ car.manufactureYear }}</p>
+        </template>
+        <template #footer>
+          <div class="flex justify-center">
+            ujnjdj
+          </div>
+        </template>
+      </CardComponent>
+    </div>
   </div>
 </template>
